26e60ea6499c2a0412f0147a854218c49fcc8d5e
bruno.fernandes <brunofernandes@softbox.com.br>
2019-07-09 22:54:01 -0300

    init

---
+++ lesson-02/webpack/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+    </div>
+  );
+};
+export default App;
---
+++ lesson-02/webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
