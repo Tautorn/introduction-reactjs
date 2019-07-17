f101d039b8f04b7946d3e8661e05548315bd6b1a
bruno.fernandes <brunofernandes@softbox.com.br>
2019-07-17 06:19:52 -0300

    list and keys

---
+++ lesson-03/list-and-keys/src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const listCustomer = [
+  {
+    id: 1,
+    name: 'Bruno Carneiro',
+    skills: ['React', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id: 2,
+    name: 'Aline Carneiro',
+    skills: ['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id: 3,
+    name: 'Fulano de Tal',
+    skills: ['Assembly']
+  },
+  {
+    id: 4,
+    name: 'José Ciclano',
+    skills: ['Reason']
+  }
+]
+
+const App = () => {
+
+  const renderCustomers = (customer, index) => {
+    return (
+      <div key={`customer-${customer.id}`}>
+        <li>{customer.name}</li>
+        {customer.skills.map(renderSkills)}
+      </div>
+    )
+  }
+
+  const renderSkills = (skill, index) => {
+    return (
+      <div style={{ paddingLeft: '30px' }} key={`skill-${index}`}>
+        <li>{skill}</li>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <div>
+        <ul>
+          {listCustomer.map(renderCustomers)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default App;
